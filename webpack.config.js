7682837cd032cdac1b2cbc59c248a280b925a9ef
James <jamespfarrell@gmail.com>
2019-03-05 14:23:03 +0100

    Change to jsx

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,17 @@ module.exports = {
         },
       },
       {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['transform-class-properties', 'react-hot-loader/babel'],
+          },
+        },
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
